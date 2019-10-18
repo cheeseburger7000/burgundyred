0c711586b87b7cce212ed86b37fd836df73867ad
shaohsiung <yangshaoxiong5545@gmail.com>
2019-10-18 11:10:41 +0800

    初始化JWT拦截器

---
+++ burgundyred-web/src/main/java/com/shaohsiung/burgundyred/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.shaohsiung.burgundyred.config;
+
+import com.shaohsiung.burgundyred.interceptor.JwtInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JwtInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/register")
+                .excludePathPatterns("/user/login")
+                .excludePathPatterns("/static/**");
+    }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/static/**")
+//                .addResourceLocations("classpath:/static/");
+//    }
+}
---
+++ burgundyred-web/src/main/java/com/shaohsiung/burgundyred/interceptor/JwtInterceptor.java
@@ -0,0 +1,29 @@
+package com.shaohsiung.burgundyred.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class JwtInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("拦截了一个请求...");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
